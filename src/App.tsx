11070df61d196168072447262c3437b776487666
RenataARocha <programadorawebrenatarocha@gmail.com>
2026-03-02 23:00:55 -0300

    refactor: extract form into EmployeeForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
+import EmployeeForm from "./components/EmployeeForm"
+
 function App() {
   return (
     <div>
-      <h1>Cadastro de funcionários</h1>
+      <h1>Meu formulário</h1>
 
-      <section className="container">
-        <form>
-          <label htmlFor="nome-completo">Nome:</label>
-          <input type="text" name="name" id="nome-completo" placeholder="nome completo" />
-
-          <label htmlFor="cpf">CPF:</label>
-          <input type="text" name="cpf" inputMode="numeric" id="cpf" placeholder="digite sem cpf" />
-
-          <label htmlFor="email">E-mail:</label>
-          <input type="email" name="email" id="email" placeholder="digite seu email" />
-
-          <label htmlFor="cargo">Cargo:</label>
-          <input type="text" name="role" id="cargo" placeholder="digite seu cargo" />
-
-          <button type="submit">Cadastrar</button>
-        </form>
-
-      </section>
+      <EmployeeForm />
     </div>
+
   )
 }
 
---
+++ src/components/EmployeeForm.tsx
@@ -0,0 +1,28 @@
+function EmployeeForm() {
+    return (
+        <div>
+            <h1>Cadastro de funcionários</h1>
+
+            <section className="container">
+                <form>
+                    <label htmlFor="nome-completo">Nome:</label>
+                    <input type="text" name="name" id="nome-completo" placeholder="nome completo" />
+
+                    <label htmlFor="cpf">CPF:</label>
+                    <input type="text" name="cpf" inputMode="numeric" id="cpf" placeholder="digite sem cpf" />
+
+                    <label htmlFor="email">E-mail:</label>
+                    <input type="email" name="email" id="email" placeholder="digite seu email" />
+
+                    <label htmlFor="cargo">Cargo:</label>
+                    <input type="text" name="role" id="cargo" placeholder="digite seu cargo" />
+
+                    <button type="submit">Cadastrar</button>
+                </form>
+
+            </section>
+        </div>
+    )
+}
+
+export default EmployeeForm;
